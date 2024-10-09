53bf75250e209e39142e2a44a240dd948c09dc15
Lợi <loivantran10012002@gmail.com>
2024-10-09 23:31:06 +0700

    add tailwind

--- 08-jobify-v2/client/src/main.tsx
+++ 08-jobify-v2/client/src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')
+createRoot(root!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
