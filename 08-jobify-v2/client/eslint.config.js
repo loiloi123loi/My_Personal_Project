c18a64a899d174f142fe756911eb5007cd521a53
Lợi <loivantran10012002@gmail.com>
2024-11-15 00:31:54 +0700

    create fe structure

--- 08-jobify-v2/client/eslint.config.js
+++ 08-jobify-v2/client/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginImport from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
@@ -17,7 +18,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: eslintPluginPrettier
+      prettier: eslintPluginPrettier,
+      import: eslintPluginImport
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -33,7 +35,18 @@ export default tseslint.config(
           useTabs: false,
           singleQuote: true,
           printWidth: 120,
-          jsxSingleQuote: true
+          jsxSingleQuote: false
+        }
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
         }
       ]
     }
